234e9912983252844278d42f3dbdaef6c28c9531
ofabriciofarias <fabriciosf@ufpa.br>
2020-05-04 07:59:07 -0300

    Create exemplo10.php

---
+++ exemplo10.php
@@ -0,0 +1,18 @@
+<?php
+
+$vetor = array();
+
+print_r($vetor);
+echo "<br>";
+
+//índice é a questão
+$vetor[0] = 10;
+
+print_r($vetor);
+echo "<br>";
+
+$vetor[1] = 20;
+
+print_r($vetor);
+echo "<br>";
+?>
